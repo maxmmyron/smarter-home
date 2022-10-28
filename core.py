15bafc08ce320b42248532b85883008222fd0786
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-10-28 13:05:11 +0100

    update consts

--- core.py
+++ core.py
@@ -1,17 +1,23 @@
 # primary loop function. Runs once per second and
 # checks for event requests from users or schedule breakpoints
-runtime = 0
+import time
+
+# running total of time running, in ms.
+_runtime = 0
+
+# delta time in ms. specifies delay between loop calls.
+_delta = 16
 
 
 # primary loop function
-def loop(delta):
-    runtime += delta
+def loop():
+    _runtime += _delta
 
 
 # primary system function. handles init of system. akin to "turn on" event.
 def sys():
-    running = True
-    delta = 16
+    _is_running = True
 
-    while running:
-        loop(delta)
+    while _is_running:
+        loop()
+        time.sleep(_delta / 1000)
