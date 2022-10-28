242da72ff0cd64867bd0ff5ba324056a58219b52
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-10-28 12:58:48 +0100

    implement basic engine loop

---
+++ core.py
@@ -0,0 +1,20 @@
+# primary loop function. Runs once per second and
+# checks for event requests from users or schedule breakpoints
+from os import wait
+
+runtime = 0
+
+# primary loop function
+
+
+def loop(delta):
+    runtime += delta
+
+
+# primary system function. handles init of system. akin to "turn on" event.
+def sys():
+    running = True
+    delta = 16
+
+    while running:
+        loop(delta)
