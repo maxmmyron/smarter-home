9aa248464b8c75a25c03ccc999eec3416196ca10
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-01 18:47:02 +0000

    update loop logic, implement debug breakpoint

--- core.py
+++ core.py
@@ -1,25 +1,39 @@
 # primary loop function. Runs once per second and
 # checks for event requests from users or schedule breakpoints
-import time
 
-import graphman
+# when running program, exit by holding down "q" key.
+
+import time
+import keyboard
 
 # total of time running from start of loop
 _runtime = 0
 
 # delta time in ms. specifies delay between loop calls.
-_delta = 16
+_delta = 500
+
+# system state. tracks whether or not system is currently idling or updating home state.
+# 0 - "idle"
+# 1 - "update"
+sys_state = 0
+
+# primary loop functionq
 
 
-# primary loop function
 def loop():
+    global _runtime
     _runtime += _delta
+    print("running: time elapsed: " + str(_runtime))
+    if sys_state:
+        print("updating sys state")
+    else:
+        print("system idling")
 
     # TODO: implement state update logic
 
 
-# primary system function. handles init of system. akin to "turn on" event.
-def sys():
+# primary system function. handles init of system. akin to "turning on" smart home.
+def sys_init():
     # init system
     _is_running = True
 
@@ -28,3 +42,11 @@ def sys():
     while _is_running:
         loop()
         time.sleep(_delta / 1000)
+        # read for q input to exit
+        if keyboard.is_pressed('q'):
+            _is_running = False
+            print("exiting system")
+
+
+# run system
+sys_init()
