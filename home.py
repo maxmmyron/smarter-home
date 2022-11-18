f2f969af48985e4f2f1cac7d05a4c4a251afbf5d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-18 20:56:09 +0000

    implement home object structure and tests

--- home.py
+++ home.py
@@ -1,2 +1,68 @@
 # home.py
 # handles home state class and general home state logic
+
+class Home:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+
+    # adds a new room to the home. if the room already exists, an exception is raised
+    def add_room(self, name, light=False, temperature=21):
+        # check if room already exists
+        for room in self.rooms:
+            if room.name == name:
+                raise Exception("Room already exists")
+
+        # add room to home
+        new_room = Room(name, light, temperature)
+        self.rooms.append(new_room)
+
+    # updates the home state to match closer to the target state
+    # throws an exception if the target state does not match the structure of the home state
+    def update(self, target):
+        # ensure home state and target state have same structure
+        if self.name != target.name:
+            raise Exception("Home state name does not match target state name")
+        if len(self.rooms) != len(target.rooms):
+            raise Exception(
+                "Home state room count does not match target state room count")
+
+        for i in range(len(self.rooms)):
+            if self.rooms[i].name != target.rooms[i].name:
+                raise Exception(
+                    "Room state name does match target state name at index " + str(i))
+
+        has_updated = False
+
+        # iterate through rooms and attempt to change state
+        for i in range(len(self.rooms)):
+            state_room = self.rooms[i]
+            target_room = target.rooms[i]
+
+            # has_updated will turn (and remain) True if any room has updated
+            has_updated = has_updated or state_room.update(target_room)
+
+        return has_updated
+
+
+class Room:
+    # creates a new room with a name, light boolean, and temperature
+    def __init__(self, name, light, temperature):
+        self.name = name
+
+        self.light = light
+        self.temperature = temperature
+
+    # updates the room state to match closer to the target room state
+    def update(self, target):
+        if (self.light == target.light and self.temperature == target.temperature):
+            return False
+
+        # change state of room
+
+        self.__setattr__("light", target.light)
+
+        tempChange = 1 if target.temperature > self.temperature else -1
+        self.__setattr__("temperature", self.temperature + tempChange)
+
+        return True
--- test_home.py
+++ test_home.py
@@ -1,3 +1,71 @@
 # test_home.py
 
 import unittest
+
+from home import Home
+
+
+class TestHome(unittest.TestCase):
+    def test_home(self):
+        # build home state
+        home = Home("test_home")
+
+        self.assertEqual(home.name, "test_home")
+        self.assertEqual(home.rooms, [])
+
+    def test_add_room(self):
+        # build home state and add rooms
+        home = Home("test_home")
+        home.add_room("test_room")
+
+        self.assertEqual(home.rooms[0] in home.rooms, True)
+        self.assertEqual(home.rooms[0].name, "test_room")
+        self.assertEqual(home.rooms[0].light, False)
+        self.assertEqual(home.rooms[0].temperature, 21)
+
+    def test_add_duplicate_room(self):
+        # build home state and add rooms
+        home = Home("test_home")
+        home.add_room("test_room")
+
+        # attempt to add duplicate room
+        with self.assertRaises(Exception):
+            home.add_room("test_room")
+
+    def test_add_custom_room(self):
+        # build home state and add rooms
+        home = Home("test_home")
+        home.add_room("test_room", True, 22)
+
+        self.assertEqual(home.rooms[0] in home.rooms, True)
+        self.assertEqual(home.rooms[0].name, "test_room")
+        self.assertEqual(home.rooms[0].light, True)
+        self.assertEqual(home.rooms[0].temperature, 22)
+
+    def test_state_change(self):
+        # build home state and add rooms
+        home = Home("test_home")
+        home.add_room("test_room", False, 20)
+
+        # build target home state
+        home_target = Home("test_home")
+        home_target.add_room("test_room", True, 22)
+
+        # attempt home state change
+        self.assertEqual(home.update(home_target), True)
+        self.assertEqual(home.rooms[0].light, True)
+        self.assertEqual(home.rooms[0].temperature, 21)
+
+        # finish state change
+        self.assertEqual(home.update(home_target), True)
+        self.assertEqual(home.rooms[0].light, True)
+        self.assertEqual(home.rooms[0].temperature, 22)
+
+        # attempt home state change to same state
+        self.assertEqual(home.update(home_target), False)
+        self.assertEqual(home.rooms[0].light, True)
+        self.assertEqual(home.rooms[0].temperature, 22)
+
+
+if __name__ == "__main__":
+    unittest.main()
