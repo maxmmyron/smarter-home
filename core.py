e427a63ef00a3c63472721a043659a55ba8fcd03
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-21 15:02:11 +0000

    outline fun

--- core.py
+++ core.py
@@ -1,2 +1,15 @@
 # core.py
 # initializes the core system and runs the primary logic loop.
+
+import datetime
+
+from home import Home
+from db import Database
+
+
+def loop():
+    pass
+
+
+def init():
+    pass
