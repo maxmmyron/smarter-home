ab4ef22193cb2deb4c1297945d38e140d023526c
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-22 02:29:26 +0000

    update core init

--- core.py
+++ core.py
@@ -5,6 +5,8 @@ import datetime
 
 from home import Home
 from db import Database
+from schedule import Schedule
+import copy
 
 db = None
 home = None
@@ -46,14 +48,68 @@ def loop():
 def init():
     global db, home, schedule
 
+    # init db
     db = Database("db")
 
+    # init home & rooms
+    # initial home state is home at 00:00
     home = Home("home")
 
+    # add rooms to home
     home.add_room("kitchen")
     home.add_room("lounge")
     home.add_room("bedroomA")
     home.add_room("bedroomB")
 
-    # set up database, home, and schedule
-    pass
+    schedule = create_schedule(home)
+
+
+def create_schedule(home):
+    # init schedule
+    schedule = Schedule()
+
+    # NOTE: we can conveniently copy the home object
+    # to create breakpoints. However since = assignment
+    # only creates a reference to the original object
+    # and we don't want to change the original home state,
+    # we need to use deepcopy() to create a copy that
+    # doesn't reference the OG home object.
+
+    # "wake up" breakpoint
+    wakeup_breakpoint = copy.deepcopy(home)  # create a deep copy
+    wakeup_breakpoint.set_room("kitchen", True, 22)
+    schedule.add_breakpoint("06:00", wakeup_breakpoint)
+
+    # "leave for work"
+    leave_breakpoint = copy.deepcopy(home)
+    leave_breakpoint.set_room("kitchen", False, 19)
+    leave_breakpoint.set_room("lounge", False, 19)
+    leave_breakpoint.set_room("bedroomA", False, 19)
+    leave_breakpoint.set_room("bedroomB", False, 19)
+    schedule.add_breakpoint("08:00", leave_breakpoint)
+
+    # "arrive from work" breakpoint
+    arrive_breakpoint = copy.deepcopy(home)
+    arrive_breakpoint.set_room("kitchen", True, 22)
+    arrive_breakpoint.set_room("lounge", True, 22)
+    arrive_breakpoint.set_room("bedroomA", False, 21)
+    arrive_breakpoint.set_room("bedroomB", False, 21)
+    schedule.add_breakpoint("17:00", arrive_breakpoint)
+
+    # "late night" breakpoint
+    late_breakpoint = copy.deepcopy(home)
+    late_breakpoint.set_room("kitchen", False, 19)
+    late_breakpoint.set_room("lounge", False, 22)
+    late_breakpoint.set_room("bedroomA", True, 22)
+    late_breakpoint.set_room("bedroomB", True, 22)
+    schedule.add_breakpoint("20:00", late_breakpoint)
+
+    # "bedtime" breakpoint
+    sleep_breakpoint = copy.deepcopy(home)
+    sleep_breakpoint.set_room("kitchen", False, 19)
+    sleep_breakpoint.set_room("lounge", False, 19)
+    sleep_breakpoint.set_room("bedroomA", False, 21)
+    sleep_breakpoint.set_room("bedroomB", False, 21)
+    schedule.add_breakpoint("22:00", sleep_breakpoint)
+
+    return schedule
