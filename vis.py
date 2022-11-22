9a99452f3d176a794b3786bb8a4da6089cbe9ab0
frogguu <91087006+frogguu@users.noreply.github.com>
2022-11-22 15:08:29 +0000

    Update vis.py
    changing vis to use home class

--- vis.py
+++ vis.py
@@ -1,9 +1,42 @@
-# vis.py
-# handles visualization setup and update logic using tkinter
+from tkinter import *
+from system import sys_init
+from home import * 
 
-class Visualiser:
-    def __init__(self):
-        '''
-        creates a new Visualiser object with the specified home.
-        '''
-        pass
+
+def graphics(home):
+    my_window = Tk()
+    my_window.title("Canvas")
+    my_window.geometry("500x500")
+
+    my_canvas = Canvas(my_window, width=500, height=500, bg="white")
+    #sitting room lights
+    if home.rooms[0].light == 1:
+        #my_canvas.create_rectangle(x1, y1, x2, y2, fill="color") 
+        my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="yellow")
+    else:
+        my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="cyan")
+
+    def tempIncrease():
+        print("hello")
+        sys_init(homeObjectList)
+        print("done")
+        my_canvas.delete("light")
+        if sitting_room_light._state == 1:
+            my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="yellow")
+        else:
+            my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="cyan")
+        my_canvas.delete("label")
+        my_canvas.create_text(200,150, text="Sitting Room", tag="label", fill="black", font=('Helvetica 15 bold'))
+        my_canvas.delete("temp")
+        my_canvas.create_text(200,220, text=str(home.rooms[0].temperature), tag="temp", fill="red", font=('Helvetica 15 bold'))
+
+    b = Button(my_window, text="Input", command=tempIncrease)
+    b.pack()
+
+    #sitting room lable
+    my_canvas.create_text(200,150, text=home.rooms[0].name, tag="label", fill="black", font=('Helvetica 15 bold'))
+    my_canvas.create_text(200,220, text=str(home.rooms[0].temperature), tags="temp", fill="red", font=('Helvetica 15 bold'))
+    my_canvas.pack()   
+    my_window.mainloop()
+
+graphics()
