bb703e0236dd8e620b690c54205958d7ce16dd29
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-22 04:17:48 +0000

    add boilerplate

--- vis.py
+++ vis.py
@@ -1,2 +1,9 @@
 # vis.py
 # handles visualization setup and update logic using tkinter
+
+class Visualiser:
+    def __init__(self):
+        '''
+        creates a new Visualiser object with the specified home.
+        '''
+        pass
