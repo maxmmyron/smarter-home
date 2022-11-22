ca7e5bd0f65ee80226e376ea573c7ab4e963d2ce
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-22 01:26:03 +0000

    outeline core routine

--- core.py
+++ core.py
@@ -6,10 +6,54 @@ import datetime
 from home import Home
 from db import Database
 
+db = None
+home = None
+schedule = None
+
+# runtime stats
+runtime = 0
+day_length = 24000
+delta = 500  # eqiv to 30 min increments
+day = datetime.date.today()
+
 
 def loop():
-    pass
+    global runtime, day, day_length, delta, db, home, schedule
+
+    if (runtime - day_length > 0):
+        # update day
+        day = datetime.date.today() + datetime.timedelta(days=1)
+        db.add_date(day)
+        runtime = 0
+
+    # input_state = check_input()
+
+    # schedule_state = schedule.check_schedule(runtime)
+
+    # set target state to input state if input state is not None
+    # otherwise set target state to schedule state
+    # otherwise set target state to current state (no change)
+
+    #target = (input_state or schedule_state) or home_state
+
+    home.update(target)
+
+    db.add_usage(home.usage)
+
+    runtime += delta
 
 
 def init():
+    global db, home, schedule
+
+    db = Database("db")
+
+    home = Home("home")
+
+    home.add_room("kitchen")
+    home.add_room("lounge")
+    home.add_room("bedroomA")
+    home.add_room("bedroomB")
+
+    # set up database, home, and schedule
     pass
