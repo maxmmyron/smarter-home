d33e1789e9422e0126b60c04de335276af351d28
frogguu <91087006+frogguu@users.noreply.github.com>
2022-11-22 15:41:50 +0000

    Ported graphics function

--- u_input.py
+++ u_input.py
@@ -1,2 +1,51 @@
-# u_input.py
-# handles input from the user and passes it to the core system as a schedule override
+def sys_init(room):
+    # init system
+    _is_running = True
+ 
+    print("-------------------------------------------------------")
+    print(" Welcome to the America Moth Smart Home control System")
+    while _is_running:
+        print("-------------------------------------------------------")
+        print("1. Light System. 2. Temperature System.")
+        option = input("Please select your options: ")
+        # Light System
+        if option == "1":
+            if room.light == 0:
+                print(room.name + " light is currently off, would you like to turn it off?")
+            else: 
+                print(room.name + " light is currently on, would you like to turn it on?")
+            check = input("(Y/N): ")
+            if check =="Y" or check == "y":
+                if room.light == 0:
+                    room.light = 1
+                    print(room.name + " light has been turned off")
+                else: 
+                    room.light = 0
+                    print(room.name + " light has been turned on")
+                break
+            else:
+                break
+                
+        # Temp system
+        elif option == "2":
+            print(room.name + " temperature is currently " + str(room.temperature) + ", would you like to change?")
+            check = input("(Y/N): ")
+            if check =="Y" or check == "y":
+                setT = int(input("Please input the Temperature you want to set:"))
+                if setT >= 0 and setT <= 28:
+                    room.temperature = setT
+                    print("You have successfully set the temperature to ", str(setT))
+                else:
+                    print("Invalid, please try again.")
+                break
+            else:
+                break
+
+        # double check to exit the system
+        elif option== "q":
+            check = input("Y/N:")
+            if check =="Y" or check == "y":
+                _is_running = False
+                print("Exiting system")
+            else:
+                continue
--- vis.py
+++ vis.py
@@ -1,7 +1,7 @@
 from tkinter import *
-from system import sys_init
+from u_input import sys_init
 from home import * 
-
+from core import loop
 
 def graphics(home):
     my_window = Tk()
@@ -16,21 +16,19 @@ def graphics(home):
     else:
         my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="cyan")
 
-    def tempIncrease():
-        print("hello")
-        sys_init(homeObjectList)
-        print("done")
+    def kitchenInput():
+        sys_init(home.rooms[0])
         my_canvas.delete("light")
-        if sitting_room_light._state == 1:
+        if home.rooms[0].light == 1:
             my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="yellow")
         else:
             my_canvas.create_rectangle(100, 100, 300, 300, tag="light", fill="cyan")
         my_canvas.delete("label")
-        my_canvas.create_text(200,150, text="Sitting Room", tag="label", fill="black", font=('Helvetica 15 bold'))
+        my_canvas.create_text(200,150, text=home.rooms[0].name, tag="label", fill="black", font=('Helvetica 15 bold'))
         my_canvas.delete("temp")
         my_canvas.create_text(200,220, text=str(home.rooms[0].temperature), tag="temp", fill="red", font=('Helvetica 15 bold'))
 
-    b = Button(my_window, text="Input", command=tempIncrease)
+    b = Button(my_window, text="Kitchen", command=kitchenInput)
     b.pack()
 
     #sitting room lable
@@ -38,5 +36,3 @@ def graphics(home):
     my_canvas.create_text(200,220, text=str(home.rooms[0].temperature), tags="temp", fill="red", font=('Helvetica 15 bold'))
     my_canvas.pack()   
     my_window.mainloop()
-
-graphics()
