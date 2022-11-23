2752f870f8febc8427e58aef8c83af908e0ad50c
Junhao Zhang <120109321@umail.ucc.ie>
2022-11-23 13:22:41 +0000

    Try to add get_usage from Database

--- core.py
+++ core.py
@@ -128,7 +128,6 @@ class Core(tk.Tk):
 
         inits tkinter widgets and builds home from initial home object
         '''
-
         # TODO: improve vis look
 
         self.dateLabel = tk.Label(self, text="", font=("Arial 13"))
@@ -152,6 +151,8 @@ class Core(tk.Tk):
         kitchen_override.pack(side='left', padx=5, pady=5)
         dining_room_override.pack(side='left', padx=5, pady=5)
         bedroom_override.pack(side='left', padx=5, pady=5)
+        # dataButton = tk.Button(self,text="Database",command=Database("Database").get_usage(datetime.date.today()))
+        # dataButton.pack(side='left', padx=5, pady=5)
 
     def _get_user_input(self, room):
         sys_init(room)
