c484fd273ccce2066ac8952dac8f46076b3182a4
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2022-11-23 02:35:07 +0000

    refactor

--- schedule.py
+++ schedule.py
@@ -11,45 +11,20 @@ class Schedule:
         '''creates a new schedule'''
         self.breakpoints = {}
 
-    def add_breakpoint(self, time, home):
+    def set_breakpoint(self, time, home):
         '''
-        adds a new breakpoint state to the schedule at the specified time
+        defines a breakpoint state to the schedule at the specified time. if a breakpoint already exists at the specified time, it will be overwritten.
 
         time: the time of the breakpoint in the format "HH:MM"
         home: the home state to set at the specified time.
 
         raises an exception if:
         - the time is invalid
-        - a breakpoint already exists at the specified time
         '''
 
         self._validate_time(time)
-
-        if (time in self.breakpoints):
-            raise Exception("Breakpoint already exists")
 
         self.breakpoints[time] = home
-        return True
-
-    def get_breakpoint(self, time):
-        '''
-        checks the schedule for a breakpoint at the specified time
-
-        time: the time of the breakpoint in the format "HH:MM"
-
-        returns:
-        - the breakpoint, if there is a breakpoint at the specified time
-        - None, if there is no breakpoint at the specified time
-
-
-        raises an exception if the time is invalid
-        '''
-        self._validate_time(time)
-
-        if (time in self.breakpoints):
-            return self.breakpoints[time]
-
-        return None
 
     def get_last_breakpoint(self, time):
         '''
@@ -69,11 +44,12 @@ class Schedule:
         for breakpoint in self.breakpoints:
             if breakpoint <= time:
                 last_breakpoint = self.breakpoints[breakpoint]
+            if breakpoint > time:
+                break
 
         # return the last breakpoint before the specified time
         return last_breakpoint
 
-    # private: validates time string passed in when adding / getting a breakpoint
     def _validate_time(self, time):
         '''
         validates the time string passed in when adding / getting a breakpoint
@@ -84,5 +60,3 @@ class Schedule:
             datetime.datetime.strptime(time, "%H:%M")
         except ValueError:
             raise ValueError("Incorrect date format, should be HH:MM")
-
-        return True
