d84fbaf5b8c3b0b2a4f10318083f7275887e464e
frogguu <91087006+frogguu@users.noreply.github.com>
2022-12-06 13:23:28 +0000

    Added command line test back

---
+++ command_input.py
@@ -0,0 +1,51 @@
+def sys_init(room):
+    # init system
+    _is_running = True
+ 
+    while _is_running:
+        print("1. " + room.name + " Light System. 2. " + room.name + " Temperature System.")
+        option = input("Please select your option: ")
+
+        # Light System
+        if option == "1":
+            if room.light == 0:
+                print(room.name + " light is currently off, would you like to turn it on?")
+            else: 
+                print(room.name + " light is currently on, would you like to turn it off?")
+            check = input("(Y/N): ")
+            if check =="Y" or check == "y":
+                if room.light == 0:
+                    room.light = 1
+                    print(room.name + " light has been turned on, exiting system \n")
+                else: 
+                    room.light = 0
+                    print(room.name + " light has been turned off, exiting system \n")        
+            else:
+                print("Exiting system \n")
+                
+                
+        # Temp system
+        elif option == "2":
+            setT = None
+            print(room.name + " temperature is currently " + str(room.temperature) + ".")
+            while type(setT) != int:    
+                setT = input("Please input the temperature you want to set: ")
+                try:
+                    setT = int(setT)
+                except: 
+                    print("Invalid input")
+                else:
+                    if setT >= 0 and setT <= 28:
+                        room.temperature = setT
+                        print("You have successfully set the temperature to", str(setT))
+                    else:
+                        print("Invalid, please try again.")
+                    print("Exiting system \n")
+            
+
+        # double check to exit the system
+        else:
+            print("Exiting system \n")
+        
+        _is_running = False
+        return None
--- test_input.py
+++ test_input.py
@@ -2,14 +2,13 @@
 
 import unittest
 from home import *
-from user_input import sys_init
-import unittest
+from command_input import sys_init
 from unittest import mock
 
 
-class TestUserInput(unittest.TestCase):
+class TestCommandInput(unittest.TestCase):
 
-    @mock.patch('user_input.input', create=True)
+    @mock.patch('command_input.input', create=True)
     def test_light_sucess(self, answers):
         test_home = Home("test")
         test_home.add_room("test_room", True, 23)
@@ -17,7 +16,7 @@ class TestUserInput(unittest.TestCase):
         sys_init(test_home.rooms[0])
         self.assertEquals(test_home.rooms[0].light, False)
 
-    @mock.patch('user_input.input', create=True)
+    @mock.patch('command_input.input', create=True)
     def test_light_failure(self, answers):
         test_home = Home("test")
         test_home.add_room("test_room", True, 23)
@@ -25,7 +24,7 @@ class TestUserInput(unittest.TestCase):
         sys_init(test_home.rooms[0])
         self.assertEquals(test_home.rooms[0].light, True)
 
-    @mock.patch('user_input.input', create=True)
+    @mock.patch('command_input.input', create=True)
     def test_light_off(self, answers):
         test_home = Home("test")
         test_home.add_room("test_room", False, 23)
@@ -33,7 +32,7 @@ class TestUserInput(unittest.TestCase):
         sys_init(test_home.rooms[0])
         self.assertEquals(test_home.rooms[0].light, True)
 
-    @mock.patch('user_input.input', create=True)
+    @mock.patch('command_input.input', create=True)
     def test_temp(self, answers):
         test_home = Home("test")
         test_home.add_room("test_room", True, 23)
@@ -41,7 +40,7 @@ class TestUserInput(unittest.TestCase):
         sys_init(test_home.rooms[0])
         self.assertEquals(test_home.rooms[0].temperature, 15)
 
-    @mock.patch('user_input.input', create=True)
+    @mock.patch('command_input.input', create=True)
     def test_temp_negative_input(self, answers):
         test_home = Home("test")
         test_home.add_room("test_room", True, 23)
@@ -49,7 +48,7 @@ class TestUserInput(unittest.TestCase):
         sys_init(test_home.rooms[0])
         self.assertEquals(test_home.rooms[0].temperature, 23)
 
-    @mock.patch('user_input.input', create=True)
+    @mock.patch('command_input.input', create=True)
     def test_temp_non_number_input(self, answers):
         test_home = Home("test")
         test_home.add_room("test_room", True, 23)
